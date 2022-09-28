a960684081564b2f9dc2c3bbe4b682a9da20931a
Jacky Rusly <jackyrusly@gmail.com>
2022-09-28 16:57:26 +0700

    Invalid

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Admin CI Test Update
+      Admin CI Test Update Invalid
 
       <Link to={`about`}>Go to About</Link>
     </div>
