1712fae1046c123295ec4296565172c137d094ef
Jacky Rusly <jackyrusly@gmail.com>
2022-09-28 10:43:02 +0700

    Push

---
+++ packages/admin/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Admin
+    </div>
+  )
+}
+
+export default App
---
+++ packages/buyer/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      Buyer
+    </div>
+  )
+}
+
+export default App
